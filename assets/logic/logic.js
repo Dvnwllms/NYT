c47396bdc159f7aa2d5e4586d5c4c9b830ed6bd7
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-02-23 13:19:15 -0500

    added divs for ease of styling

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -13,15 +13,17 @@ $.ajax({
     var articles=result.response.docs ;
   console.log(articles);
   for(var i = 0; i < articles.length; i++){
-      var headline= $("<h1>").text(articles[i].headline.main);
-      console.log(articles[i].headline.main);
-      var byline= $("<h2>").text(articles[i].byline.original);
-      console.log(articles[i].byline.original);
-      var date= $("<p>").text(articles[i].pub_date);
-      console.log(articles[i].pub_date);
-      var url=$("<p>").text(articles[i].web_url);
-      console.log(articles[i].web_url);
-    $("#searchResults").append(headline, byline, date, url);
+    var resultDiv = $("<div>")
+    var headline= $("<h1>").text(articles[i].headline.main);
+    console.log(articles[i].headline.main);
+    var byline= $("<h2>").text(articles[i].byline.original);
+    console.log(articles[i].byline.original);
+    var date= $("<p>").text(articles[i].pub_date);
+    console.log(articles[i].pub_date);
+    var url=$("<p>").text(articles[i].web_url);
+    console.log(articles[i].web_url);
+    resultDiv.append(headline, byline, date, url);
+    $("#searchResults").append(resultDiv);
     console.log("loop");
   }
 }).fail(function(err) {
